184be9b28435b53d93793770ca9587f1a36b7e38
Benchuk Dima <dimitron400033@gmail.com>
2024-08-24 12:56:21 +0300

    Выполненный второй урок

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 let title, screens, screenPrice, rollback, fullPrice, adaptive;
 
-alert("Начало пути");
+title = "JS course";
+screens = "Простые, Сложные, Интерактивные";
+screenPrice = 100;
+rollback = 50;
+fullPrice = 1000;
+adaptive = true;
 
-console.log("Всё получится");
+console.log(typeof title);
+console.log(typeof fullPrice);
+console.log(typeof adaptive);
+
+console.log(screens.length);
+
+console.log(screenPrice + "$");
+console.log(fullPrice + "$");
+
+console.log(screens.toLowerCase().split(", "));
+
+console.log(fullPrice * (rollback / 100) + "$");

8908cc9fc3dd455bfeceea0b2f78979be1696978
Benchuk Dima <dimitron400033@gmail.com>
2024-08-26 01:17:34 +0300

    Выполнил третий урок

--- script.js
+++ script.js
@@ -1,5 +1,38 @@
-let title, screens, screenPrice, rollback, fullPrice, adaptive;
+let rollback = 50;
 
-alert("Начало пути");
+let title = prompt("Как называется ваш проект?");
+let screens = prompt("Как типы экранов нужно разработать?", "Простые, Сложные, Интерактивные");
+let screenPrice = +prompt("Сколько будет стоить данная работа?", 12000);
+let adaptive = prompt("Нужен ли адаптив на сайте?", "Да/Нет");
 
-console.log("Всё получится");
+if (adaptive.toLowerCase() == "да") {
+    adaptive = true;
+} else {
+    adaptive = false;
+}
+
+let service1 = prompt("Какой дополнительный тип услуги нужен?");
+let servicePrice1 = +prompt("Сколько это будет стоить?", 100);
+let service2 = prompt("Какой дополнительный тип услуги нужен?");
+let servicePrice2 = +prompt("Сколько это будет стоить?", 100);
+
+let fullPrice = screenPrice + servicePrice1 + servicePrice2;
+let servicePercentPrice = fullPrice - fullPrice * rollback / 100;
+
+console.log(fullPrice);
+console.log(servicePercentPrice);
+
+switch (true) {
+    case fullPrice >= 30000:
+        console.log("Даем скидку в 10%");
+        break
+    case fullPrice >= 15000 && fullPrice < 30000:
+        console.log("Даем скидку в 5%");
+        break
+    case fullPrice >= 0 && fullPrice < 15000:
+        console.log("Скидка не предусмотрена");
+        break
+    case fullPrice < 0:
+        console.log("Что то пошло не так");
+        break
+}

06023dbf02b629d5de08c5f62778c95a66b394db
Benchuk Dima <dimitron400033@gmail.com>
2024-08-26 18:06:58 +0300

    Выполнил практику 5 урока

--- script.js
+++ script.js
@@ -2,16 +2,32 @@
 
 let rollback = 50;
 
-let title = prompt("Как называется ваш проект?");
-let screens = prompt("Как типы экранов нужно разработать?", "Простые, Сложные, Интерактивные");
-let screenPrice = +prompt("Сколько будет стоить данная работа?", 12000);
-let adaptive = confirm("Нужен ли адаптив на сайте?");
-let service1 = prompt("Какой дополнительный тип услуги нужен?");
-let servicePrice1 = +prompt("Сколько это будет стоить?", 100);
-let service2 = prompt("Какой дополнительный тип услуги нужен?");
-let servicePrice2 = +prompt("Сколько это будет стоить?", 100);
+let title;
+let screens;
+let screenPrice;
+let adaptive;
+let service1;
+let service2;
+let allServicePrices;
+let fullPrice;
+let servicePercentPrice;
 
-let allServicePrices, fullPrice, servicePercentPrice;
+const isNumber = function (num) {
+    return !isNaN(parseFloat(num)) && isFinite(num);
+}
+
+const asking = function () {
+    title = prompt("Как называется ваш проект?", "Верстка по кайфу");
+    screens = prompt("Как типы экранов нужно разработать?", "Простые, Сложные, Интерактивные");
+
+    screenPrice = prompt("Сколько будет стоить данная работа?", 12000);
+
+    while (!isNumber(screenPrice)) {
+        screenPrice = prompt("Сколько будет стоить данная работа?", 12000);
+    }
+
+    adaptive = confirm("Нужен ли адаптив на сайте?");
+}
 
 const showTypeOf = function (variable) {
     return variable + ", " + typeof variable;
@@ -35,22 +51,37 @@ const getTitle = function () {
 }
 
 const getAllServicePrices = function () {
-    return servicePrice1 + servicePrice2;
+    let sum = 0;
+
+    for (let i = 0; i < 2; i++) {
+
+        if (i === 0) {
+            service1 = prompt("Какой дополнительный тип услуги нужен?");
+        } else if (i === 1) {
+            service2 = prompt("Какой дополнительный тип услуги нужен?");
+        }
+
+        sum += +prompt("Сколько это будет стоить?", 1000);
+    }
+
+    return sum;
 }
 
 const getFullPrice = function () {
-    return screenPrice + allServicePrices;
+    return +screenPrice + allServicePrices;
 }
 
 const getServicePercentPrices = function () {
     return fullPrice - fullPrice * rollback / 100;
 }
 
+asking();
 allServicePrices = getAllServicePrices();
 fullPrice = getFullPrice();
 servicePercentPrice = getServicePercentPrices();
+title = getTitle();
 
-console.log(showTypeOf(getTitle()));
+console.log(showTypeOf(title));
 console.log(showTypeOf(fullPrice));
 console.log(showTypeOf(adaptive));
 

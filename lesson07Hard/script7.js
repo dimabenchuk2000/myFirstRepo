e8eef0c736d3d9809c3e1013963895f5bab38dc3
Benchuk Dima <dimitron400033@gmail.com>
2024-09-01 13:02:09 +0300

    Выполнил 7-ое усложненное задание

--- lesson07Hard/script7.js
+++ lesson07Hard/script7.js
@@ -1,3 +1,23 @@
 let week = ["Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье"];
+let day = new Date().getDay();
 
-console.log(week);
+if (day === 0) {
+    day = 6;
+} else {
+    day -= 1;
+}
+
+for (let i in week) {
+    if (week[i] === "Суббота" || week[i] === "Воскресенье") {
+        week[i] = week[i].italics();
+    }
+
+    if (i === day.toString()) {
+        week[i] = week[i].bold();
+    }
+
+    document.body.innerHTML += week[i] + "<br>";
+}
+
+console.log(day);
+
